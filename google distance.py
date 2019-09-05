0a74eaf6687daf210edfefb85bfc11b8be1e2c70
ABHINAV JHA <abhinav98jha@gmail.com>
2019-09-05 23:23:54 +0530

    Update google distance.py

--- google distance.py
+++ google distance.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import time
 import json
 
-API_key = "AIzaSyB7rwlVcuzoG7YfpAPzbZ8fPNT8GdihZlw"
+API_key = ""
 
 def distance_calc(sources, destinations):
     """
